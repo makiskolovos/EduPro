c120fa5f0397eeae8a07f1e60514aae83a64dafe
LAlexandros <81259319+LAlexandros@users.noreply.github.com>
2021-06-10 20:47:28 +0300

    Create Professor_ClassController.java

---
+++ src/main/Professor/Professor_ClassController.java
@@ -0,0 +1,37 @@
+package main.professor;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Professor_ClassController {
+    @FXML
+    private Button logout_p;
+    @FXML
+    private Button home_p;
+    @FXML
+    private Button class_p;
+    @FXML
+    private Button grade_p;
+    @FXML
+    private Button messages_p;
+    @FXML
+    private Button notes_p;
+    @FXML
+    private Button announcements_p;
+    @FXML
+    private Button forum_p;
+    @FXML
+    private Button profile_p;
+    @FXML
+    private Button students_id;
+    @FXML
+    private Button professor_id;
+  
