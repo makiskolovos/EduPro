e1a0b08f945a1c4883c6fbfdcc67c19d25466ceb
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:39:07 +0300

    Update AnnouncementsController.java

--- src/main/Student/AnnouncementsController.java
+++ src/main/Student/AnnouncementsController.java
@@ -13,3 +13,26 @@ import javafx.stage.Stage;
 import main.DbConnection;
 
 import java.io.IOException;
+public class AnnouncementsController {
+    @FXML
+    private Button logout;
+    @FXML
+    private Button home_id;
+    @FXML
+    private Button class_id;
+    @FXML
+    private Button grade_id;
+    @FXML
+    private Button messages_id;
+    @FXML
+    private Button notes_id;
+    @FXML
+    private Button announcements_id;
+    @FXML
+    private Button forum_id;
+    @FXML
+    private Button guide_id;
+    @FXML
+    private Button partner_id;
+    @FXML
+    private Button profile_id;
