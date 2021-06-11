19eaa562287c57ac4227b94b6528e47b89593ae6
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:49:59 +0300

    Update ForumController.java

--- src/main/Student/ForumController.java
+++ src/main/Student/ForumController.java
@@ -12,3 +12,32 @@ import javafx.stage.Stage;
 
 
 import java.io.IOException;
+
+public class ForumController {
+    @FXML
+    private Button logout;
+    @FXML
+    private Button home_id;
+    @FXML
+    private Button class_id;
+    @FXML
+    private Button grade_id;
+    @FXML
+    private Button messages_id;
+    @FXML
+    private Button notes_id;
+    @FXML
+    private Button announcements_id;
+    @FXML
+    private Button forum_id;
+    @FXML
+    private Button guide_id;
+    @FXML
+    private Button partner_id;
+    @FXML
+    private Button profile_id;
+
+    public TextArea main_body_id;
+    public TextField forum_subject_id;
+    @FXML
+    private Button forum_post_id;
