efb17e0e88b217338c2be76d3e45262e1ebd92fb
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:46:04 +0300

    Update FindPartnerController.java

--- src/main/Student/FindPartnerController.java
+++ src/main/Student/FindPartnerController.java
@@ -13,3 +13,43 @@ import javafx.stage.Stage;
 import main.DbConnection;
 
 import java.io.IOException;
+
+public class FindPartnerController {
+    @FXML
+    private Button logout;
+    @FXML
+    private Button home_id;
+    @FXML
+    private Button class_id;
+    @FXML
+    private Button grade_id;
+    @FXML
+    private Button messages_id;
+    @FXML
+    private Button notes_id;
+    @FXML
+    private Button announcements_id;
+    @FXML
+    private Button forum_id;
+    @FXML
+    private Button guide_id;
+    @FXML
+    private Button partner_id;
+    @FXML
+    private Button profile_id;
+
+    @FXML
+    private TableView<String> tableView;
+    @FXML
+    private TableColumn<DbConnection,String> property;
+    @FXML
+    private TableColumn <DbConnection,String> first_name;
+    @FXML
+    private TableColumn <DbConnection,String> last_name;
+    @FXML
+    private TableColumn <DbConnection,String> AM;
+    @FXML
+    private TableColumn <DbConnection,String> username;
+    @FXML
+    private TableColumn <DbConnection,String> email;
+
