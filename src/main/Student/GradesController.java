9f468a887fd4debe9c7c4824361ba39784505387
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:56:04 +0300

    Update GradesController.java

--- src/main/Student/GradesController.java
+++ src/main/Student/GradesController.java
@@ -22,3 +22,39 @@ import java.sql.*;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+public class GradesController {
+    @FXML
+    private Button logout;
+    @FXML
+    private Button home_id;
+    @FXML
+    private Button class_id;
+    @FXML
+    private Button grade_id;
+    @FXML
+    private Button messages_id;
+    @FXML
+    private Button notes_id;
+    @FXML
+    private Button announcements_id;
+    @FXML
+    private Button forum_id;
+    @FXML
+    private Button guide_id;
+    @FXML
+    private Button partner_id;
+    @FXML
+    private Button profile_id;
+    @FXML
+    private TableView <GradeTableController>tableGrades;
+    @FXML
+    private TableColumn <GradeTableController,String> subject_title_id;
+    @FXML
+    private TableColumn <GradeTableController,String> grades_id;
+    @FXML
+    private TableColumn <GradeTableController,String> student_name_grade;
+    @FXML
+    private TableColumn <GradeTableController,String> student_lname_grade;
+    @FXML
+    private TableColumn <GradeTableController,String> student_AM;
