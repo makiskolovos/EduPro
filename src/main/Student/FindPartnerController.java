502ea4f25e9cf2891a3a61703232bad67e21e7a3
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:46:21 +0300

    Update FindPartnerController.java

--- src/main/Student/FindPartnerController.java
+++ src/main/Student/FindPartnerController.java
@@ -53,3 +53,105 @@ public class FindPartnerController {
     @FXML
     private TableColumn <DbConnection,String> email;
 
+ @FXML
+    private void click_logout(ActionEvent actionEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/login.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Homepage");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_home(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/under_student_homepage.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Homepage");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_class(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/admin_class.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Subjects");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_grades(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/admin_grades.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - My Grades");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_messages(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/messages.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Messages");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_notes(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/notes.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - My Notes");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_announcements(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/admin_announcements.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Announcements");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_forum(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/forum.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Forum");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_guide(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/student_guide.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Student Guide");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_partner(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/find_partner.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - Find Partner");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+    @FXML
+    private void click_profile(MouseEvent mouseEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getClassLoader().getResource("main/students/under_student_profile.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("EDU PRO - My Profile");
+        stage.setScene(new Scene(part, 1200, 674));
+        stage.show();
+    }
+
+
+
+}
