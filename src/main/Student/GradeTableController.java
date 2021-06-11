cbff21a7c2b62c84658572d12e62539f08c57dcb
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:57:23 +0300

    Update GradeTableController.java

--- src/main/Student/GradeTableController.java
+++ src/main/Student/GradeTableController.java
@@ -1,1 +1,12 @@
+package main.students;
 
+public class GradeTableController {
+
+    String st_name, st_lastname, st_AM, subject_title, grade;
+    public  GradeTableController(String st_name, String st_lastname, String st_AM, String subject_title, String grade){
+        this.st_name = st_name;
+        this.st_lastname = st_lastname;
+        this.st_AM = st_AM;
+        this.subject_title = subject_title;
+        this.grade = grade;
+    }
