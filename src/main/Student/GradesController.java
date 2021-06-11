db1cbb98838755dea47aab962c5e98c416b29c50
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:55:47 +0300

    Update GradesController.java

--- src/main/Student/GradesController.java
+++ src/main/Student/GradesController.java
@@ -1,1 +1,24 @@
+package main.students;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import main.DbConnection;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
