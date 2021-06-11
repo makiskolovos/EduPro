459b69049c9a6791930e0f78727b48453a32cc40
ElenaChaidou <79599303+ElenaChaidou@users.noreply.github.com>
2021-06-11 17:45:46 +0300

    Update FindPartnerController.java

--- src/main/Student/FindPartnerController.java
+++ src/main/Student/FindPartnerController.java
@@ -1,1 +1,15 @@
+package main.students;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import main.DbConnection;
+
+import java.io.IOException;
